6aea71a4e7f6ca5f5533ce4d8d070098e35604af
JãoZin <jpzuaneti@hotmail.com>
2025-10-31 03:17:25 +0000

    quero adicionar o pixel do meta ads, adicione no lugar certo! : <!-- Met

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,8 +1,9 @@
 import Script from 'next/script';
+import React from 'react';
 
 // IMPORTANT: REPLACE THE PLACEHOLDER VALUES BELOW WITH YOUR ACTUAL TRACKING IDS
 const GA_TRACKING_ID = 'G-XXXXXXXXXX'; // Your Google Analytics Tracking ID
-const META_PIXEL_ID = 'YOUR_META_PIXEL_ID'; // Your Meta Pixel ID
+const META_PIXEL_ID = '800912026200250'; // Your Meta Pixel ID
 
 export function Analytics() {
   // Don't render scripts if IDs are placeholders
@@ -10,7 +11,7 @@ export function Analytics() {
   const isMetaConfigured = META_PIXEL_ID !== 'YOUR_META_PIXEL_ID';
 
   return (
-    <>
+    <React.Fragment>
       {isGaConfigured && (
         <>
           <Script
@@ -35,25 +36,35 @@ export function Analytics() {
       )}
 
       {isMetaConfigured && (
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${META_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <>
+          <Script
+            id="meta-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        </>
       )}
-    </>
+    </React.Fragment>
   );
 }
