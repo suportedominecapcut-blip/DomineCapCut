68eaa59ef18bfe3df28590c9cc9c9179bcc405ba
JãoZin <jpzuaneti@hotmail.com>
2025-11-04 00:50:12 +0000

    troque por essas imagens de depoimento nao adicione lik ingur e sim o ca

--- src/lib/testimonial-images.ts
+++ src/lib/testimonial-images.ts
@@ -1,17 +1,17 @@
 export const testimonialImages = [
   {
     id: 'depoimento-1',
-    src: 'https://i.imgur.com/i9CrsB1.png',
+    src: 'https://i.imgur.com/83a483d.png',
     alt: 'Depoimento de aluno satisfeito com o curso Domine o CapCut',
   },
   {
     id: 'depoimento-2',
-    src: 'https://i.imgur.com/zWz5Pj7.png',
+    src: 'https://i.imgur.com/yN403cR.png',
     alt: 'Depoimento de aluna satisfeita com o curso Domine o CapCut',
   },
   {
     id: 'depoimento-3',
-    src: 'https://i.imgur.com/jEofA2s.png',
+    src: 'https://i.imgur.com/X2pcr5o.png',
     alt: 'Depoimento de aluno satisfeito com o curso Domine o CapCut',
   },
 ];
