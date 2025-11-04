dcb0200bff3f630fae3370a4fc373ba181473b50
JãoZin <jpzuaneti@hotmail.com>
2025-11-04 00:48:49 +0000

    nao use link ingur nas fotos de depoimento use as imagens que mandei

--- src/lib/testimonial-images.ts
+++ src/lib/testimonial-images.ts
@@ -1,17 +1,17 @@
 export const testimonialImages = [
   {
     id: 'depoimento-1',
-    src: 'https://i.imgur.com/kKpyfJb.png',
+    src: 'https://i.imgur.com/i9CrsB1.png',
     alt: 'Depoimento de aluno satisfeito com o curso Domine o CapCut',
   },
   {
     id: 'depoimento-2',
-    src: 'https://i.imgur.com/kKpyfJb.png',
-    alt: 'Depoimento de aluno satisfeito com o curso Domine o CapCut',
+    src: 'https://i.imgur.com/zWz5Pj7.png',
+    alt: 'Depoimento de aluna satisfeita com o curso Domine o CapCut',
   },
   {
     id: 'depoimento-3',
-    src: 'https://i.imgur.com/kKpyfJb.png',
+    src: 'https://i.imgur.com/jEofA2s.png',
     alt: 'Depoimento de aluno satisfeito com o curso Domine o CapCut',
   },
 ];
